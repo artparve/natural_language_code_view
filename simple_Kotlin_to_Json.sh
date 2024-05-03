0a7eac0564e01100b6f8d5afc9a8d2e240254850
artparve <artparve@gmail.com>
2024-05-03 06:52:01 +0300

    Task done

---
+++ simple_Kotlin_to_Json.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# Create a function to add parameters to json
+function add_parametrs() {
+    output_buffer=$output_buffer"\"parameters\": [ { "
+    for (( k = 3; k < $#; k += 2)); do
+      output_buffer=$output_buffer"\"name\": \"${!k}\", "
+      next_index=$((k + 1))
+      output_buffer=$output_buffer"\"type\": \"${!next_index}\" "
+    done
+    output_buffer=$output_buffer"} ], "
+}
+
+# Check if the file is given an argument for the script
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+
+# Get the file extension
+filename="$1"
+extension="${filename##*.}"
+
+# Check if the extension is "kt"
+if [ "$extension" != "kt" ]; then
+    echo "$filename does not have .kt extension."
+fi
+
+# Create an output file in json format
+output_filename="output_${filename%.*}.json"
+output_buffer=""
+
+
+output_buffer=$output_buffer"{"
+mapfile -t file_contents < "$filename"
+end_of_file=""
+
+
+for (( i = 0; i < ${#file_contents[@]}; i++ )); do
+  if [[ ${file_contents[i]} =~ "fun" ]]; then
+    IFS=$'\n'
+    parts=($(echo "${file_contents[i]}" | awk -F '[ (): {]' '{for(i=1; i<=NF; i++) print $i}'))
+    number_of_parts="${#parts[@]}"
+    count_of_brackets=0
+    nested_flag=0
+
+
+    output_buffer=$output_buffer" \"declarations\": [ { "
+    output_buffer=$output_buffer"\"type\": \"function\", "
+    output_buffer=$output_buffer"\"name\": \"${parts[1]}\", "
+
+    add_parametrs "${parts[@]}"
+
+    if [ $(( $number_of_parts % 2 )) -eq 0 ]; then
+      output_buffer=$output_buffer"\"returnType\": \"Unit\","
+    else
+      output_buffer=$output_buffer"\"returnType\": \"${parts[-1]}\","
+    fi
+
+    output_buffer=$output_buffer" \"body\": \""
+
+    for (( j = i; j < ${#file_contents[@]}; j++ )); do
+#      echo "${file_contents[j]}"
+#      echo "$nested_flag"
+      if [[ ${file_contents[j]} =~ "fun" ]]; then
+        nested_flag=$((nested_flag + 1))
+      fi
+      output_buffer=$output_buffer"$(echo "${file_contents[j]}" | sed -e 's/^[ \t]*//' | sed 's/"/\\"/g')"
+
+      if [[ ${file_contents[j]} =~ "{" ]]; then
+        count_of_brackets=$((count_of_brackets + 1))
+      fi
+
+      if [[ ${file_contents[j]} =~ "}" ]]; then
+        count_of_brackets=$((count_of_brackets - 1))
+      fi
+
+      if [ $count_of_brackets -eq 0 ]; then
+        end_of_file=" } ]"$end_of_file
+        output_buffer=$output_buffer"\","
+        if [ $nested_flag -eq 1 ]; then
+          output_buffer=$output_buffer"$end_of_file"
+          end_of_file=""
+        fi
+        break
+      fi
+
+    done
+
+  fi
+done
+
+#truncate -s -1 $output_filename
+output_buffer=$output_buffer" }"
+
+echo "$output_buffer" | sed 's/\(.*\),/\1 /' > "$output_filename"
+
+
+echo -e "Done\nSee file $output_filename"
---
+++ test2.kt
@@ -0,0 +1,80 @@
+package cub.async.answers
+
+import cub.async.tasks.Server
+import cub.async.tasks.posts
+import kotlinx.coroutines.*
+import java.lang.Runnable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+import kotlin.system.measureTimeMillis
+
+fun getWorkers(): ExecutorService {
+    val cores = Runtime.getRuntime().availableProcessors()
+    println("#cores: $cores")
+    return Executors.newFixedThreadPool(cores * 8)
+}
+
+fun solutionUsingAsync() {
+    val myAsync = MyAsyncFramework()
+    for (text in posts) {
+        myAsync.async({ Server.getToken() }) { token ->
+            Server.submitPost(text, token)
+        }
+    }
+    Thread.sleep(500) // ;^)
+    myAsync.close()
+}
+
+fun threadsSolution() {
+    val threadPool = getWorkers()
+
+    for (text in posts) {
+        val runnable = Runnable {
+            val token = Server.getToken()
+            val meta = Server.submitPost(text, token)
+        }
+        threadPool.execute(runnable)
+    }
+
+    threadPool.shutdown()
+    threadPool.awaitTermination(1, TimeUnit.HOURS)
+    println("All posts processed")
+}
+
+suspend fun submitPostNicely(text: String, token: Server.Token) =
+    withContext(Dispatchers.IO) {
+        Server.submitPost(text, token)
+    }
+
+fun coroutinesSolution() {
+    runBlocking {
+        coroutineScope {
+            for (text in posts) {
+                launch(Dispatchers.IO) {
+                    val token = suspendCoroutine { it.resume(Server.getToken()) }
+                    val meta = submitPostNicely(text, token)
+                    // val meta = suspendCoroutine { it.resume(Server.submitPost(text, token)) }
+                }
+            }
+        }
+    }
+    println("All posts processed")
+}
+
+fun main() {
+    val timeThreads = measureTimeMillis {
+        threadsSolution()
+    }
+    println("It took ${timeThreads.toDouble() / posts.size} on average")
+    val timeCoroutines = measureTimeMillis {
+        coroutinesSolution()
+    }
+    println("It took ${timeCoroutines.toDouble() / posts.size} on average")
+    val timeAsync = measureTimeMillis {
+        solutionUsingAsync()
+    }
+    println("It took ${timeAsync.toDouble() / posts.size} on average")
+}
